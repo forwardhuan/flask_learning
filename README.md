98ced24b25dae4aee255433f3ee0c173bc19d6fc
forward_huan <forward_m@126.com>
2020-10-30 17:44:42 +0800

    user manager

---
+++ README.md
@@ -0,0 +1,11 @@
+https://www.cnblogs.com/wupeiqi
+
+flask.pocoo.org/extensions/
+
+PythonWeb框架
+- django 大而全，重武器，内部提供： ORM， Admin， 中间件， Form， ModelForm， Session， 缓存， 信号， CSRF
+- flask 短小精悍， 可扩展性强, 第三方组件丰富
+- tornado， 短小精悍，异步非阻塞
+- 其它
+   - web.py
+   - bottle.py
---
+++ settings.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+
+class Config:
+    # mysql+pymysql://user:password@hostip:port/databasename
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:root@127.0.0.1:3306/flaskday06'
+    # 是否有警告
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    # 是否是调试模式
+    SQLALCHEMY_ECHO = True
+    SECRET_KEY = "fdfdsfdsfksf"
+
+
+class DevelopmentConfig(Config):
+    ENV = 'development'
+    DEBUG = True
+
+
+class ProductionConfig(Config):
+    ENV = 'production'
+    DEBUG = False
